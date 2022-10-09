340030e5d38117c1ce3853f425cfc290056723f0
Tristan Suñer <87903221+Triswhatever@users.noreply.github.com>
2022-10-09 15:49:56 +0800

    Add files via upload

--- Basic Queuing Cashier/CashierWindowQueueForm.cs
+++ Basic Queuing Cashier/CashierWindowQueueForm.cs
@@ -18,11 +18,15 @@ namespace Basic_Queuing_Cashier
         {
             InitializeComponent();
             Timer timer = new Timer();
-            timer.Interval = (1 * 1000); // 1 sec.
-            timer.Tick += new EventHandler(btnRefresh_Click); //timer1_tick represents the name of Tick Event
+            timer.Interval = (1 * 1000); 
+            timer.Tick += new EventHandler(btnRefresh_Click); 
             timer.Start();
         }
-
+        
+        Boolean FormOpen = false;
+        CustomerView customerView = new CustomerView();
+        FormCollection AllForms = Application.OpenForms;
+        Form newForm = new Form();
         private void btnRefresh_Click(object sender, EventArgs e)
         {
             DisplayCashierQueue(CashierClass.CashierQueue);
@@ -37,10 +41,49 @@ namespace Basic_Queuing_Cashier
                 listCashierQueue.Items.Add(obj.ToString());
             }
         }
+        public delegate void PassControl(object sender);
+        public PassControl passControl;
 
+        public void OpenCashier()
+        {
+            if (FormOpen == false)
+            {
+                CashierWindowQueue cashierWindowQueue = new CashierWindowQueue();
+                cashierWindowQueue.Visible = true;
+                FormOpen = true;
+            }
+        }
+        public void Next()
+        {
+            foreach (Form form in AllForms)
+            {
+                if (form.Name == "CustomerView")
+                {
+                    newForm = form;
+                    FormOpen = true;
+                }
+            }
+            if (FormOpen == true)
+            {
+                if (passControl != null)
+                {
+                    customerView.CurrentServe.Text = CashierClass.CashierQueue.Peek();
+                    CashierClass.CashierQueue.Dequeue();
+                    passControl(customerView.CurrentServe);
+                }
+                
+            }
+            else
+            {
+                customerView.ShowDialog();
+                customerView.CurrentServe.Text = CashierClass.CashierQueue.Peek().ToString();
+                CashierClass.CashierQueue.Dequeue();
+            }
+        }
         private void btnNext_Click(object sender, EventArgs e)
         {
-            CashierClass.CashierQueue.Dequeue();
+            Next();
+            
         }
     }
 }
---
+++ Basic Queuing Cashier/CustomerView.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Basic_Queuing_Cashier
+{
+    partial class CustomerView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ServingLabel = new System.Windows.Forms.Label();
+            this.CurrentServe = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ServingLabel
+            // 
+            this.ServingLabel.AutoSize = true;
+            this.ServingLabel.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ServingLabel.Location = new System.Drawing.Point(177, 41);
+            this.ServingLabel.Name = "ServingLabel";
+            this.ServingLabel.Size = new System.Drawing.Size(213, 45);
+            this.ServingLabel.TabIndex = 0;
+            this.ServingLabel.Text = "*Now Serving";
+            // 
+            // CurrentServe
+            // 
+            this.CurrentServe.AutoSize = true;
+            this.CurrentServe.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CurrentServe.Location = new System.Drawing.Point(214, 157);
+            this.CurrentServe.Name = "CurrentServe";
+            this.CurrentServe.Size = new System.Drawing.Size(121, 48);
+            this.CurrentServe.TabIndex = 1;
+            this.CurrentServe.Text = "label2";
+            // 
+            // CustomerView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 345);
+            this.Controls.Add(this.CurrentServe);
+            this.Controls.Add(this.ServingLabel);
+            this.Name = "CustomerView";
+            this.Text = "CustomerView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label ServingLabel;
+        public Label CurrentServe;
+    }
+}
