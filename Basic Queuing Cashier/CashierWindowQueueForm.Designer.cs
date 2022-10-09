d563f36bf8300a38b55e826106ef1f9dfe770b5c
Rei <Rei@DESKTOP-5SNHRHO>
2022-10-09 12:00:42 +0800

    Add project files.

---
+++ Basic Queuing Cashier/CashierWindowQueueForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Basic_Queuing_Cashier
+{
+    partial class CashierWindowQueue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.listCashierQueue = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(21, 29);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(112, 59);
+            this.btnRefresh.TabIndex = 0;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(21, 104);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(112, 59);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // listCashierQueue
+            // 
+            this.listCashierQueue.Location = new System.Drawing.Point(163, 31);
+            this.listCashierQueue.Name = "listCashierQueue";
+            this.listCashierQueue.Size = new System.Drawing.Size(426, 394);
+            this.listCashierQueue.TabIndex = 2;
+            this.listCashierQueue.UseCompatibleStateImageBehavior = false;
+            // 
+            // CashierWindowQueue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 450);
+            this.Controls.Add(this.listCashierQueue);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnRefresh);
+            this.Name = "CashierWindowQueue";
+            this.Text = "CashierWindowQueueForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnRefresh;
+        private Button btnNext;
+        private ListView listCashierQueue;
+    }
+}
---
+++ Basic Queuing Cashier/CashierWindowQueueForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace Basic_Queuing_Cashier
+{
+    public partial class CashierWindowQueue : Form
+    {
+        public CashierWindowQueue()
+        {
+            InitializeComponent();
+            Timer timer = new Timer();
+            timer.Interval = (1 * 1000); // 1 sec.
+            timer.Tick += new EventHandler(btnRefresh_Click); //timer1_tick represents the name of Tick Event
+            timer.Start();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayCashierQueue(CashierClass.CashierQueue);
+
+        }
+
+        public void DisplayCashierQueue(IEnumerable CashierList)
+        {
+            listCashierQueue.Items.Clear();
+            foreach (Object obj in CashierList)
+            {
+                listCashierQueue.Items.Add(obj.ToString());
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            CashierClass.CashierQueue.Dequeue();
+        }
+    }
+}
